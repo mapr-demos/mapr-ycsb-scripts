3b0cce877e6a28cb7c0746c10b4c71d5fd44c1bd
Cloud User <centos@ip-10-0-0-104.us-west-1.compute.internal>
2017-05-20 00:23:24 +0000

    updates

--- tools/client_prepmapr.sh
+++ tools/client_prepmapr.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 source $MAPR_YCSB_HOME/env.sh
+EPELREPO='http://download.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-9.noarch.rpm'
+PKGADD='yum -y install'
 TFILE=$(mktemp)
 
 cat << EOF > $TFILE
@@ -21,19 +23,42 @@ EOF
 clush -g $CLUSH_NODE_GROUP -c $TFILE --dest=/home/centos/maprtech.repo
 
 clush -g $CLUSH_NODE_GROUP -c /home/centos/jre-8u60-linux-x64.rpm --dest=/home/centos/
-clush --options='-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo yum -y localinstall jre-8u60-linux-x64.rpm
+clush --options='-t -t' -g \
+    $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo yum -y localinstall jre-8u60-linux-x64.rpm
 
-clush --options='-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo cp maprtech.repo /etc/yum.repos.d/
-clush --options='-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo yum -y install mapr-client.x86_64
+clush --options='-t -t' -g \
+    $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo cp maprtech.repo /etc/yum.repos.d/
+clush --options='-t -t' -g \
+    $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo yum -y install mapr-client.x86_64
+
+# add some useful packages
+clush --options='-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD dstat iperf wget
+clush -o '-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER wget $EPELREPO
+clush -o '-t -t' -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER sudo rpm -Uvh epel-release-7*.rpm
 
 # copy over the tools
 clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -c $TOOL_HOME --dest=$REMOTE_COPYTO
 clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -c $YCSB_HOME --dest=$REMOTE_COPYTO
 
-# copy over the MapR-DB jars 
-clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER rm $YCSB_HOME/hbase10-binding/lib/hbase-c\*.jar
-clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER cp /opt/mapr/hbase/hbase-$MAPR_HBASE_VERSION/lib/hbase-c\* \
-	$YCSB_HOME/hbase10-binding/lib
+# copy over the MapR-DB jars -- pick one of the db nodes as the source
+# because these are not installed on the client
+D=$(mktemp -d)
+F=$(clush -N -q -g $CLUSH_DB_NODE_GROUP --pick=1 ls /opt/mapr/hbase/hbase-$MAPR-HBASE-VERSION/lib/hbase-c\*)
+for j in $F; do
+	clush -N -q -g $CLUSH_DB_NODE_GROUP --pick=1 --rcopy $j --dest=$D
+done
+
+# delete the old jars first
+clush -g $CLUSH_NODE_GROUP -l \
+    $SSH_REMOTE_USER rm -f $YCSB_HOME/hbase10-binding/lib/hbase-c\*.jar
+
+for file in $D/*; do
+	NEWF=$(echo $file | sed -e 's/.jar.*/.jar/')
+	mv $file $NEWF
+	clush -g $CLUSH_NODE_GROUP -l \
+		$SSH_REMOTE_USER -c $NEWF \
+		--dest=$YCSB_HOME/hbase10-binding/lib
+done
 
 # a bunch of sysctls recommended by eng.
 clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -o '-t -t' \
@@ -50,3 +75,7 @@ clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -o '-t -t' \
     'sudo sysctl -w net.ipv4.tcp_wmem=16777216'
 clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -o '-t -t' \
     'sudo sysctl -w net.ipv4.tcp_mem="16777216 16777216 16777216"'
+
+# take in the new conf
+clush -g $CLUSH_NODE_GROUP -l $SSH_REMOTE_USER -o '-t -t' sudo sysctl --system
+

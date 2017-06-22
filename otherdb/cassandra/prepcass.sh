d980dcd14c48ff0e35c335a57b456749a3626268
Cloud User <centos@ip-10-0-0-104.us-west-1.compute.internal>
2017-06-22 02:05:56 +0000

    update EPEL and add option to force feed the ephemeral disks instead of detecting them

--- otherdb/cassandra/prepcass.sh
+++ otherdb/cassandra/prepcass.sh
@@ -7,16 +7,17 @@
 # assumes Centos 7.x and cassandra 3.10
 
 source $MAPR_YCSB_HOME/env.sh
-DISKS="nvme0n1"
+DISKS="/dev/nvme0n1 /dev/nvme1n1 /dev/nvme2n1 /dev/nvme3n1"
 PKGADD='yum -y install'
 REMOTE_COPYTO='/home/centos'
 CASS_PKG="http://apache.org/dist/cassandra/3.10/apache-cassandra-3.10-bin.tar.gz"
+EPELREPO='https://dl.fedoraproject.org/pub/epel/epel-release-latest-7.noarch.rpm'
 
 # set to i.e. 'md0' if using RAID below
-CASS_DISK="nvme0n1p1"
+CASS_DISK="md0"
 
 # set this to md0 if RAIDing disks together
-DO_RAID=false
+DO_RAID=true
 
 # local files to be copied to cluster machines
 CASS_CONFIG_FILE=$TOOL_HOME/otherdb/cassandra/cassandra.yaml
@@ -27,88 +28,100 @@ SYSCTL_CONF_FILE=$TOOL_HOME/otherdb/cassandra/sysctl.conf
 
 # install java 8, maven and a few other pkgs
 echo "installing prereqs"
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo $PKGADD java-1.8.0-openjdk wget dstat
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo wget \
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD wget dstat
+clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER wget $EPELREPO
+clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo rpm -Uvh epel-release-*.rpm
+clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
+    'wget --no-cookies --no-check-certificate --header \
+	"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
+	http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm'
+clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD_LOCAL jdk-8u131-linux-x64.rpm
+
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo wget \
 	http://repos.fedorapeople.org/repos/dchen/apache-maven/epel-apache-maven.repo -O \
         /etc/yum.repos.d/epel-apache-maven.repo
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo $PKGADD apache-maven
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD apache-maven
 
 # optionally RAID0 the disks together
 if [ "$DO_RAID" = "true" ]; then
 	echo "preparing ephemeral disks"
-	clush -a -l $SSH_REMOTE_USER -c $RAID_SCRIPT --dest=/tmp/
-	clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo bash /tmp/raid_ephemeral.sh
+	clush -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER -c $RAID_SCRIPT --dest=/tmp/
+	clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP \
+	    -l $SSH_REMOTE_USER sudo bash /tmp/raid_ephemeral.sh -d \"$DISKS\"
 fi
 
 # add cassandra user and group
 echo "making users"
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo groupadd cassandra
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo adduser -g cassandra cassandra
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo groupadd cassandra
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo adduser -g cassandra cassandra
 
 # install cassandra binary distribution
 echo "downloading and installing files"
-clush --options='-t -t' -a -l $SSH_REMOTE_USER wget $CASS_PKG
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo tar xvfz apache-cassandra-3.10-bin.tar.gz -C /mnt
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo chgrp -R cassandra /mnt/apache-cassandra-3.10
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo chown -R cassandra /mnt/apache-cassandra-3.10
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER wget $CASS_PKG
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo tar xvfz apache-cassandra-3.10-bin.tar.gz -C /mnt
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo chgrp -R cassandra /mnt/apache-cassandra-3.10
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo chown -R cassandra /mnt/apache-cassandra-3.10
 
 # uncomment this to use packages instead
-# clush --options='-t -t' -a -l $SSH_REMOTE_USER \
+# clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
 # 'echo "deb http://www.apache.org/dist/cassandra/debian 39x main" \
 # 	| sudo tee -a /etc/apt/sources.list.d/cassandra.sources.list'
 
 # disable swap
 echo "disabling swap"
-clush --options='-t -t' -a -l $SSH_REMOTE_USER sudo swapoff --all
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo swapoff --all
 
 # put in a host entry for the internal interface
 echo "adding a host entry"
-clush -a --options='-t -t' -l $SSH_REMOTE_USER \
+clush -g $CLUSH_DB_NODE_GROUP --options='-t -t' -l $SSH_REMOTE_USER \
     "hostname -i | xargs echo casshost | awk ' { print \$2, \$1 } ' | sudo tee -a /etc/hosts"
 
 # distribute the config file
 echo "distributing config file"
-clush -l $SSH_REMOTE_USER -a -c $CASS_CONFIG_FILE --dest=/tmp/cass.new
-clush -l $SSH_REMOTE_USER -a --options='-t -t' \
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $CASS_CONFIG_FILE --dest=/tmp/cass.new
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP --options='-t -t' \
     sudo cp /mnt/apache-cassandra-3.10/conf/cassandra.yaml /tmp/cassandra.bak
-clush -l $SSH_REMOTE_USER -a --options='-t -t' \
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP --options='-t -t' \
     sudo cp /tmp/cass.new /mnt/apache-cassandra-3.10/conf/cassandra.yaml
 
 # set proper limits
 echo "(un)setting limits"
-clush -a -l $SSH_REMOTE_USER -c $LIMITS_FILE --dest=/tmp/cassandra-limits.conf
-clush --options='-t -t' -a -l $SSH_REMOTE_USER \
+clush -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER -c $LIMITS_FILE --dest=/tmp/cassandra-limits.conf
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
     'sudo cp /etc/security/limits.conf /tmp/baklimits.conf'
-clush --options='-t -t' -a -l $SSH_REMOTE_USER \
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
     'sudo cp /tmp/cassandra-limits.conf /etc/security/limits.conf'
-clush --options='-t -t' -a -l $SSH_REMOTE_USER 'sudo sysctl -p'
-clush -l $SSH_REMOTE_USER -a -c $CASS_SYSCTL_FILE --dest=/tmp/cassandra.conf
-clush --options='-t -t' -a -l $SSH_REMOTE_USER 'sudo cp /tmp/cassandra.conf /etc/sysctl.d/'
-clush -l $SSH_REMOTE_USER -a -c $SYSCTL_CONF_FILE --dest=/tmp/sysctl.conf
-clush --options='-t -t' -a -l $SSH_REMOTE_USER 'sudo cp /tmp/sysctl.conf /etc/sysctl.conf'
-clush --options='-t -t' -a -l $SSH_REMOTE_USER 'sudo sysctl --system'
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER 'sudo sysctl -p'
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $CASS_SYSCTL_FILE --dest=/tmp/cassandra.conf
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER 'sudo cp /tmp/cassandra.conf /etc/sysctl.d/'
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $SYSCTL_CONF_FILE --dest=/tmp/sysctl.conf
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER 'sudo cp /tmp/sysctl.conf /etc/sysctl.conf'
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER 'sudo sysctl --system'
 
 # copy over all the scripts and tools
 echo "copying scripts and tools"
-clush -l $SSH_REMOTE_USER -a -c $YCSB_HOME --dest=$YCSB_HOME
-clush -l $SSH_REMOTE_USER -a -c $TOOL_HOME --dest=$TOOL_HOME
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $YCSB_HOME --dest=$YCSB_HOME
+clush -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $TOOL_HOME --dest=$TOOL_HOME
 
 # set optimal IO parameters for RAID device or disk
-clush --options='-t -t' -a -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK \
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK \
 	echo 1 | sudo tee /sys/block/$CASS_DISK/queue/nomerges'
-clush --options='-t -t' -a -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK 'echo 8 | \
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK 'echo 8 | \
     sudo tee /sys/block/$CASS_DISK/queue/read_ahead_kb'
-clush --options='-t -t' -a -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK 'echo deadline | \
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP \
+    -l $SSH_REMOTE_USER export CASS_DISK=$CASS_DISK 'echo deadline | \
     sudo tee /sys/block/$CASS_DISK/queue/scheduler'
 
 # turn off transparent huge page compaction
-clush --options='-t -t' -a -l $SSH_REMOTE_USER 'echo never | \
-    sudo tee /sys/kernel/mm/transparent_hugepage/defrag | sudo tee -a /etc/rc.local'
+clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER 'echo never | \
+    sudo tee /sys/kernel/mm/transparent_hugepage/defrag | \
+    sudo tee /sys/kernel/mm/transparent_hugepage/enabled | \
+    sudo tee -a /etc/rc.local'
 
 # copy over the tools
-clush a -l $SSH_REMOTE_USER -a -c $TOOL_HOME --dest=$REMOTE_COPYTO
-clush a -l $SSH_REMOTE_USER -a -c $YCSB_HOME --dest=$REMOTE_COPYTO
+clush a -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $TOOL_HOME --dest=$REMOTE_COPYTO
+clush a -l $SSH_REMOTE_USER -g $CLUSH_DB_NODE_GROUP -c $YCSB_HOME --dest=$REMOTE_COPYTO
 
 # start cassandra
-clush --options='-t -t' -a -l $SSH_REMOTE_USER \
+#clush --options='-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
     'nohup sudo -u cassandra /mnt/apache-cassandra-3.10/bin/cassandra'
--- otherdb/cassandra/raid_ephemeral.sh
+++ otherdb/cassandra/raid_ephemeral.sh
@@ -8,6 +8,18 @@
 
 METADATA_URL_BASE="http://169.254.169.254/2012-01-12"
 
+USEDISKS=""
+while [[ $# -gt 1 ]]
+do
+    key="$1"
+    case $key in
+    -d|--disks)
+        USEDISKS="$2"
+        shift # past argument
+	;;
+    esac
+done
+
 yum -y -d0 install mdadm curl
 
 # Configure Raid - take into account xvdb or sdb
@@ -21,37 +33,49 @@ else
   DRIVE_SCHEME='sd'
 fi
 
+# if the user hasn't passed any disks,
 # figure out how many ephemerals we have by querying the metadata API, and then:
 #  - convert the drive name returned from the API to the hosts DRIVE_SCHEME, if necessary
 #  - verify a matching device is available in /dev/
 drives=""
 ephemeral_count=0
-ephemerals=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/ | grep ephemeral)
-for e in $ephemerals; do
-  echo "Probing $e .."
-  device_name=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/$e)
-  # might have to convert 'sdb' -> 'xvdb'
-  device_name=$(echo $device_name | sed "s/sd/$DRIVE_SCHEME/")
-  device_path="/dev/$device_name"
-
-  # test that the device actually exists since you can request more ephemeral drives than are available
-  # for an instance type and the meta-data API will happily tell you it exists when it really does not.
-  if [ -b $device_path ]; then
-    echo "Detected ephemeral disk: $device_path"
-    drives="$drives $device_path"
-    ephemeral_count=$((ephemeral_count + 1 ))
-  else
-    echo "Ephemeral disk $e, $device_path is not present. skipping"
-  fi
-done
-
-if [ "$ephemeral_count" = 0 ]; then
-  echo "No ephemeral disk detected. exiting"
-  exit 0
+if [ "$USEDISKS" != "" ]; then
+    echo "using supplied disk list: " $USEDISKS
+    drives=$USEDISKS
+    for d in $drives; do
+        ephemeral_count=$((ephemeral_count + 1 ))
+    done
+else
+    ephemerals=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/ | grep ephemeral)
+    for e in $ephemerals; do
+      echo "Probing $e .."
+      device_name=$(curl --silent $METADATA_URL_BASE/meta-data/block-device-mapping/$e)
+      # might have to convert 'sdb' -> 'xvdb'
+      device_name=$(echo $device_name | sed "s/sd/$DRIVE_SCHEME/")
+      device_path="/dev/$device_name"
+    
+      # test that the device actually
+      # exists since you can request more ephemeral drives than are available
+      # for an instance type and the meta-data API will
+      # happily tell you it exists when it really does not.
+      if [ -b $device_path ]; then
+        echo "Detected ephemeral disk: $device_path"
+        drives="$drives $device_path"
+        ephemeral_count=$((ephemeral_count + 1 ))
+      else
+        echo "Ephemeral disk $e, $device_path is not present. skipping"
+      fi
+    done
+    
+    if [ "$ephemeral_count" = 0 ]; then
+      echo "No ephemeral disk detected. exiting"
+      exit 0
+    fi
+    
+    # ephemeral0 is typically mounted for us already. umount it here
+    umount /mnt
 fi
 
-# ephemeral0 is typically mounted for us already. umount it here
-umount /mnt
 
 # overwrite first few blocks in case there is a filesystem, otherwise mdadm will prompt for input
 for drive in $drives; do
@@ -63,14 +87,14 @@ mdadm --create --verbose /dev/md0 --level=0 -c256 --raid-devices=$ephemeral_coun
 echo DEVICE $drives | tee /etc/mdadm.conf
 mdadm --detail --scan | tee -a /etc/mdadm.conf
 blockdev --setra 65536 /dev/md0
-#mkfs -t ext3 /dev/md0
-mkfs.xfs -s size=4096 -d sw=6 -d su=262144 /dev/md0
-#mount -t ext3 -o noatime /dev/md0 /mnt
+mkfs.xfs -s size=4096 -d sw=$ephemeral_count -d su=262144 /dev/md0 
 mount -t xfs -o noatime /dev/md0 /mnt
 
 # Remove xvdb/sdb from fstab
-chmod 777 /etc/fstab
-sed -i "/${DRIVE_SCHEME}b/d" /etc/fstab
+if [ "$USEDISKS" == "" ]; then
+    chmod 777 /etc/fstab
+    sed -i "/${DRIVE_SCHEME}b/d" /etc/fstab
+fi
 
 # Make raid appear on reboot
 echo "/dev/md0 /mnt xfs noatime 0 0" | tee -a /etc/fstab

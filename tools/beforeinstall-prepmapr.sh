e726fec752c6f5ddafcc94138342e7ccc1378da9
Cloud User <centos@ip-10-0-0-104.us-west-1.compute.internal>
2017-08-01 20:38:19 +0000

    fix typo

--- tools/beforeinstall-prepmapr.sh
+++ tools/beforeinstall-prepmapr.sh
@@ -48,7 +48,7 @@ clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER \
     'wget --no-cookies --no-check-certificate --header \
 	"Cookie: gpw_e24=http%3A%2F%2Fwww.oracle.com%2F; oraclelicense=accept-securebackup-cookie" \
 	http://download.oracle.com/otn-pub/java/jdk/8u131-b11/d54c1d3a095b4ff2b6607d096fa80163/jdk-8u131-linux-x64.rpm'
-clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD_LOCAL jre-8u131-linux-x64.rpm
+clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo $PKGADD_LOCAL jdk-8u131-linux-x64.rpm
 
 # add mapr user
 clush -o '-t -t' -g $CLUSH_DB_NODE_GROUP -l $SSH_REMOTE_USER sudo groupadd -g 5000 mapr
